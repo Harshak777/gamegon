906b441afff76f28b157da507f869f75aac8aa25
Harshak777 <Harshak777@users.noreply.github.com>
2021-12-05 12:56:05 +0530

    corrected bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
@@ -6,11 +6,25 @@ import Typography from '@material-ui/core/Typography';
 import HomeScreen from "./containers/HomeScreen";
 import GamesArena from "./containers/GamesArena";
 
+import Web3 from 'web3';
+
 const App = () => {
+
+    const [account, setAccount] = useState("");
+
+    async function loadBlockChain() {
+        // console.log(web3);
+        const web3 = new Web3(Web3.givenProvider);
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+    }
+
+    useEffect(() => loadBlockChain, []);
+
     return (
         <div>
             <Router>
-                <Route exact path="/" component={HomeScreen} />
+                <Route exact path="/" component={HomeScreen} account={account} />
                 <Route path="/join" component={GamesArena} />
             </Router>
         </div>
--- client/src/containers/HomeScreen/HomeScreen.jsx
+++ client/src/containers/HomeScreen/HomeScreen.jsx
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { useHistory } from "react-router-dom";
-import web3 from '../../components/web3';
-// import Web3 from 'web3';
+// import web3 from '../../components/web3';
+import Web3 from 'web3';
 
 import HomeScreenStyles from './HomeScreenStyles';
 
-const HomeScreen = () => {
-    const [account, setAccount] = useState("");
-
-    async function loadBlockChain() {
-        console.log(web3)
-        // const web3 = new Web3(Web3.givenProvider);
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
-    }
-
-    useEffect(() => loadBlockChain, []);
+const HomeScreen = (props) => {
+    
 
     const history = useHistory();
     const classes = HomeScreenStyles();
@@ -29,7 +20,7 @@ const HomeScreen = () => {
                 justifyContent="space-evenly"
                 alignItems="center" spacing={2}>
                 <Grid item>
-                    <h1>Your Account {account}</h1>
+                    <h1>Your Account {props.account}</h1>
                 </Grid>
                 <Grid item>
                     <TextField
